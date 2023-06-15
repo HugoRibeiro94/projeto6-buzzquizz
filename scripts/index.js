24deee145972e56c8ab5a8e6bc427cea8903fe16
hugo <hugoo.ribeiro03@gmail.com>
2023-06-15 11:21:12 -0300

    Buscar quizzes no servidor e renderizarização

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,40 @@
 // Autorização do Token
 axios.defaults.headers.common['Authorization'] = 'Wzez4a3YIDoCsVRbeXAYFCmh';
+
+// Buscar quizzes no servidor e renderizar
+let quizzes = [];
+
+function carregarQuizzes(){
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes',);
+    promise.then(quizzesRecebidos);
+}
+carregarQuizzes()
+
+// Receber quizzes vindo do servidor
+function quizzesRecebidos(res){
+    console.log(res.data);
+    quizzes = res.data;
+
+    renderizarQuizzes()
+}
+
+// Mostrar quizzes no HTML 
+function renderizarQuizzes(){
+
+    const containerQuizz = document.querySelector('.container-quizzes');
+    containerQuizz.innerHTML = '';
+
+    for( let i = 0; i < quizzes.length; i++){
+
+        let quizz = quizzes[i];
+
+        containerQuizz.innerHTML += `
+            <div class="exemplo-quizz">
+                <img src='${quizzes[i].image}'>
+                <p>${quizzes[i].title}</p>
+            </div>
+        ` 
+    }
+    console.log(containerQuizz);
+}
