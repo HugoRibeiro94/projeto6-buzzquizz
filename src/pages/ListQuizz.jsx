978d46f2078ddb72fdb53696d2ff4c86a3250d10
Hugo <hugoo.ribeiro03@gmail.com>
2024-07-04 21:00:23 -0300

    teste get axios

---
+++ src/pages/ListQuizz.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function ListQuizz(){
+
+    const config = {
+        headers: { Authorization :`${import.meta.env.API_KEY}`}
+    }
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes",config);
+        promise.then(resposta => {
+            console.log(resposta.data);
+        });
+        promise.catch( erro => console.log(erro) );
+    },[]);
+
+    return(
+        <>
+      
+        </>
+    )
+}
