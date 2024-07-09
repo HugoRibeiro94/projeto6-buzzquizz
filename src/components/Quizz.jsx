ce11fcb162157d5f3c42e4e86e5444bafd44c4e3
Hugo <hugoo.ribeiro03@gmail.com>
2024-07-09 19:55:49 -0300

    feat: quizz

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -4,7 +4,11 @@ export default function Quizz(props){
     console.log(props);
     return(
         <>
-           <Img><img src={props.quizz.image}/></Img>
+           <Img>
+                <img src={props.quizz.image}/>
+                <label>{props.quizz.title}</label>
+            </Img>
+            
         </>
     )
 }
@@ -13,5 +17,16 @@ const Img = styled.div`
     img{
         width: 100%;
         height: 227px;
+    }
+    label{
+        font-family: Roboto;
+        font-size: 19px;
+        font-weight: 400;
+        line-height: 22.27px;
+        text-align: left;
+        color: rgba(255, 255, 255, 1);
+        position: absolute;
+        top: 125px;
+        left: 25%;
     }
 `
